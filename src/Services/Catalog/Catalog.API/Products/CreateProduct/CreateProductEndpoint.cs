69cee9bcbb4abe5db39e1b5a819a27ecd8051f8a
Marko Perica <marko.perica2@gmail.com>
2024-06-18 02:38:37 +0200

    Create handler and endpoint

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,36 @@
 namespace Catalog.API.Products.CreateProduct;
 
-public class CreateProductEndpoint
+public record CreateProductRequest(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price)
 {
-    
+}
+
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost(
+            pattern: "/products",
+            handler: async (CreateProductRequest request, ISender sender) =>
+            {
+                CreateProductCommand command = request.Adapt<CreateProductCommand>();
+
+                CreateProductResult? result = await sender.Send(request: command);
+
+                CreateProductResponse? response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created(uri: $"/products/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(statusCode: StatusCodes.Status201Created)
+            .ProducesProblem(statusCode: StatusCodes.Status400BadRequest)
+            .WithSummary(summary: "Create Product")
+            .WithDescription("Create Product");
+    }
 }

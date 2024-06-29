b6cb443a222556b52ab2758673418ea4f70afb6e
Marko Perica <marko.perica2@gmail.com>
2024-06-29 20:12:09 +0200

    Add exceptions

---
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/BadRequestException.cs
@@ -0,0 +1,13 @@
+namespace BuildingBlocks.Exceptions;
+
+public class BadRequestException : Exception
+{
+    public BadRequestException(string message): base(message: message) { }
+
+    public BadRequestException(string message, string details) : base(message: message)
+    {
+        Details = details;
+    }
+
+    public string? Details { get; }
+}
---
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+namespace BuildingBlocks.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string message) : base(message: message) { }
+
+    public NotFoundException(string name, object key) : base(message: $"Entity \"{name}\" ({key}) was not found.") { }
+}
